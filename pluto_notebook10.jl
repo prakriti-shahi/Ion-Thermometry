5d7c30389da6bd1576e07a43ebd9fb2e704a2704
prakriti-shahi <71139715+prakriti-shahi@users.noreply.github.com>
2026-03-09 13:30:32 -0400

    Add files via upload

---
+++ pluto_notebook10.jl
@@ -0,0 +1,29 @@
+### A Pluto.jl notebook ###
+# v0.20.20
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 93f066b2-c0f8-11f0-b56e-df703b628a76
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.12.0"
+manifest_format = "2.0"
+project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═93f066b2-c0f8-11f0-b56e-df703b628a76
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
